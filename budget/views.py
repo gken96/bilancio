7127258a441d7d7427d873d0e4065b5bc09c33e1
deva <devanandu96@gmail.com>
2017-03-19 20:35:48 +0530

    list ready

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import buy_list
 # Create your views here.
 def post_list(request):
-    return render(request, 'budget/post_list.html', {})
+    posts = buy_list.objects.all()
+    return render(request, 'budget/post_list.html', {'posts':posts})
